af72530a6d1f23d378e937d28fd6b8745ea37550
Alexander van Saase <avsaase@gmail.com>
2026-08-11 22:24:35 +0200

    Add more docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,77 @@
+//! Conversions between [`chrono`] and [`jiff`] types.
+//!
+//! `chrono` is in the process of being soft-deprecated in favor of `jiff`. For small codebases,
+//! it usually makes sense to migrate all at once. For larger codebases, a full migration may not
+//! be feasible in one go, and it can make more sense to migrate incrementally, converting between
+//! `chrono` and `jiff` types at the boundaries as you go. Even once your own code is fully on
+//! `jiff`, some dependencies may still expose `chrono` types in their public APIs, so conversions
+//! are still needed at those boundaries. This crate provides those conversions, exposed as the
+//! [`ToJiff`]/[`TryToJiff`] traits for infallible conversions and [`ToChrono`]/[`TryToChrono`]
+//! traits for fallible conversions.
+//!
+//! This is a small crate, and the conversions it provides are not complicated. But whether a given
+//! conversion is fallible or infallible isn't always obvious from the two APIs alone, so it can
+//! still be worth using this crate rather than hand-rolling the conversions yourself.
+//!
+//! # Supported versions
+//!
+//! Support for each major version of `chrono` and `jiff` is gated behind its own Cargo feature.
+//! Currently these are the supported versions:
+//!
+//! | Feature    | Crate     | Version |
+//! |------------|-----------|---------|
+//! | `chrono04` | `chrono`  | `0.4`   |
+//! | `jiff02`   | `jiff`    | `0.2`   |
+//!
+//! Enabling the features for a given version of `chrono` and `jiff` will enable the conversions
+//! between those versions. Currently the `chrono04` and `jiff02` features are enabled by default.
+//!
+//! Once `jiff` reaches `1.0` support for this version can be added to this crate.
+//!
+//! [`chrono`]: https://docs.rs/chrono
+//! [`jiff`]: https://docs.rs/jiff
+
 #[cfg(all(feature = "chrono04", feature = "jiff02"))]
 pub mod chrono04_jiff02;
 
-/// Trait for infallible conversions from `chrono` types to `jiff` types.
-pub trait ToJiff<T> {
-    fn to_jiff(&self) -> T;
+/// Infallible conversions from `chrono` types to `jiff` types.
+pub trait ToJiff<J> {
+    /// Convert this `chrono` type to the corresponding `jiff` type.
+    ///
+    /// This conversion is infallible, so it will always succeed.
+    fn to_jiff(&self) -> J;
 }
 
-/// Trait for infallible conversions from `jiff` types to `chrono` types.
-pub trait ToChrono<T> {
-    fn to_chrono(&self) -> T;
+/// Infallible conversions from `jiff` types to `chrono` types.
+pub trait ToChrono<C> {
+    /// Convert this `jiff` type to the corresponding `chrono` type.
+    ///
+    /// This conversion is infallible, so it will always succeed.
+    fn to_chrono(&self) -> C;
 }
 
-/// Trait for fallible conversions from `chrono` types to `jiff` types.
-pub trait TryToJiff<T> {
+/// Fallible conversions from `chrono` types to `jiff` types.
+pub trait TryToJiff<J> {
+    /// The error type for this conversion.
     type Error;
 
-    fn to_jiff(&self) -> Result<T, Self::Error>;
+    /// Convert this `chrono` type to the corresponding `jiff` type.
+    ///
+    /// This conversion is fallible,. If the converion fails, an error of type `Self::Error` will
+    /// be returned. The reasons why the conversion can fail is document on the implementation of
+    /// this trait for the specific `chrono` type.
+    fn to_jiff(&self) -> Result<J, Self::Error>;
 }
 
-/// Trait for fallible conversions from `jiff` types to `chrono` types.
-pub trait TryToChrono<T> {
+/// Fallible conversions from `jiff` types to `chrono` types.
+pub trait TryToChrono<C> {
+    /// The error type for this conversion.
     type Error;
 
-    fn to_chrono(&self) -> Result<T, Self::Error>;
+    /// Convert this `jiff` type to the corresponding `chrono` type.
+    ///
+    /// This conversion is fallible. If the conversion fails, an error of type `Self::Error` will
+    /// be returned. The reasons why the conversion can fail is document on the implementation of
+    /// this trait for the specific `jiff` type.
+    fn to_chrono(&self) -> Result<C, Self::Error>;
 }
